43238d4f1bd6f7e199256ede7991e23e122b5301
vah-ram <vahram.s.ghazaryan@gmail.com>
2025-11-17 02:47:06 +0400

    pushed model

--- models/UserModel.js
+++ models/UserModel.js
@@ -12,7 +12,13 @@ const UserSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
-    }
+    },
+    contacts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        }
+    ]
 },{
     timestamps: true
 });
